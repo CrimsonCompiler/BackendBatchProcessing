5a107e89452519263efe0dc6f2e3db4ca48202e0
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-14 20:37:26 +0600

    Add project files.

---
+++ BackendBatchProcessing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendBatchProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Batch Processing");
+        }
+    }
+}
